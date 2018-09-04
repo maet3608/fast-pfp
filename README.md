206ace22425ca205855980a4c7f2a2c92262deb3
Stefan Maetschke <stefanrm@au1.ibm.com>
2018-09-04 13:38:22 +1000

    working version that needs some cleaning up

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Readme
 
 Pytoch implementation of the Fast Projected Fixed-Point Algorithm (FastPFP) for graph matching introduced 
-in the following [paper](https://arxiv.org/pdf/1207.1114)
+in the following [paper](https://arxiv.org/pdf/1207.1114).
+
+
+## Related links
+
+[Graph Matching by Feng Zhou](http://www.f-zhou.com/gm.html)
+[Implementation by Emanuele Olivetti](https://github.com/emanuele/fastPFP/blob/master/fastPFP.py)
 
 
--- fastpfp/match.py
+++ fastpfp/match.py
@@ -4,75 +4,111 @@ https://arxiv.org/pdf/1207.1114
 """
 from __future__ import print_function
 
-import torch
+import torch as to
+import torch.cuda as cuda
 import numpy as np
+
+from sys import float_info
 
 
 def check_gpu():
-    print(torch.cuda.is_available())
-    print(torch.cuda.get_device_name(0))
+    print(cuda.is_available())
+    print(cuda.get_device_name(0))
 
 
-def pfp(A1, A2, dt):
-    alpha = 0.5
-    lam = 10
+def loss(A1, A2, L1, L2, P, lam=0.0):
+    from torch import mm
+    return 0.5 * (A1 - mm(P, mm(A2, P.t()))).norm() + lam * (
+                L1 - mm(P, L2)).norm()
+    # return 0.5 * norm(A1 - P.dot(A2.dot(P.T))) + lam * norm(L1 - P.dot(L2))
+
+
+def discretize(X):
+    X = X.numpy().copy()
+    min_x = X.min() - 1.
+    P = np.zeros(X.shape)
+    while (X > min_x).any():
+        r, c = np.unravel_index(X.argmax(), X.shape)
+        P[r, c] = 1.
+        X[r, :] = min_x
+        X[:, c] = min_x
+    return P
+
+
+def get_sizes(A1, A2):
     n1, m1 = A1.shape
     n2, m2 = A2.shape
     assert n1 == m1
     assert n2 == m2
     assert n1 >= n1
-    print('A1', A1.shape)
-    print('A2', A2.shape)
+    return n1, n2
 
-    o1 = torch.ones(n1, 1).type(dt)
 
-    O1 = torch.ones(n1, n2).type(dt)
-    print('O1', O1.shape)
-    O2 = torch.ones(n1, n1).type(dt)
-    print('O2', O2.shape)
-    I = torch.eye(n1).type(dt)
-    print('I', I.shape)
+def pfp(A1, A2, L1, L2, alpha=0.5, lam=1.0):
+    from torch import mm, zeros, ones, eye
+
+    threshold1 = threshold2 = 1.0e-6
+    max_iter1 = max_iter2 = 100
+    eps1 = eps2 = float_info.max
+    #dt = cuda.FloatTensor if cuda.is_available() else to.FloatTensor
+    dt = to.FloatTensor
+
+    L1 = to.Tensor(L1).type(dt)
+    L2 = to.Tensor(L2).type(dt)
+    A1 = to.Tensor(A1).type(dt)
+    A2 = to.Tensor(A2).type(dt)
+
+    n1, n2 = get_sizes(A1, A2)
+    o1 = ones(n1, 1).type(dt)
+    O1 = ones(n1, n2).type(dt)
+    O2 = ones(n1, n1).type(dt)
+    I = eye(n1).type(dt)
+    I1 = I / n1
 
     X = O1.div(n1 * n2).type(dt)
-    print('X', X.shape)
-    Y = torch.zeros(n1, n1).type(dt)
-    print('Y', Y.shape)
-    K = torch.zeros(n1, n2).type(dt)
-    print('K', K.shape)
+    Y = zeros(n1, n1).type(dt)
+    K = mm(L1, L2.t()).type(dt)
 
-    dot = torch.mm
-
-    n_i, n_j = 5, 5
-    for i in range(n_i):
-        print('i', i, '-----------------------------')
-        Y[:n1, :n2] = dot(A1, dot(X, A2)) + K.mul(lam)
-        print('  Y', Y.shape)
-        for j in range(n_j):
-            print('j', j, '---------------')
-            T = I / n1
-            T = T + dot(o1.t(), dot(Y, o1)).div(n1 * n1) * I
-            T = T - Y / n1
-            Y = Y + dot(T, O2) - dot(O2 /n1, Y)
-            #print(Y)
-        X = (1 - alpha) * X + alpha * Y[:n1, :n2]
-        X = X / X.max()
+    for _ in range(max_iter1):
+        Y[:n1, :n2] = mm(A1, mm(X, A2)) + lam * K
+        for _ in range(max_iter2):
+            T = I1 + (mm(o1.t(), mm(Y, o1)) / (n1 * n1)) * I - Y / n1
+            Ynew = Y + mm(T, O2) - mm(O2, Y) / n1
+            Ynew = (Ynew + Ynew.abs()) / 2
+            eps2 = (Ynew - Y).abs().max()
+            Y = Ynew
+            if eps2 < threshold2:
+                break
+        Xnew = (1 - alpha) * X + alpha * Y[:n1, :n2]
+        Xnew /= Xnew.max()
+        eps1 = (Xnew - X).abs().max()
+        X = Xnew
+        print('loss =', loss(A1, A2, L1, L2, X))
         print(X)
+        if eps1 < threshold1:
+            break
+    return X
 
 
 def run():
-    # dt = torch.cuda.FloatTensor if torch.cuda.is_available() else
-    # torch.FloatTensor
-    dt = torch.FloatTensor
+    L1 = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    L2 = np.array([[0, 0], [0, 1], [1, 0]])
+    A1 = np.array([[0, 1, 1, 0], [1, 0, 0, 0], [1, 0, 0, 1], [0, 0, 1, 0]])
+    A2 = np.array([[0, 1, 1], [1, 0, 0], [1, 0, 0]])
 
-    # square adjacency matrices with shape of A1 >= A2
-    # must be symmetric by can be weighted
-    A1 = torch.Tensor([[0, 1, 1, 0], [1, 0, 0, 0], [1, 0, 0, 1], [0, 0, 1, 0]])
-    A2 = torch.Tensor([[0, 1, 1], [1, 0, 0], [1, 0, 0]])
+    X = pfp(A1, A2, L1, L2, lam=0.0)
+    P = discretize(X)
 
-    pfp(A1, A2, dt)
+    R = P.dot(A2.dot(P.T))
+    D = A1 - R
+    print('*******************')
+    print(P)
+    print(X)
+    print(A1)
+    print(R)
+    print(D)
 
 
 if __name__ == '__main__':
     check_gpu()
     run()
-
